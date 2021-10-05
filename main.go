486fae120f362641ae8881a0e6cead93a88519da
Spidey03 <sayhai2naveenkumar@gmail.com>
2021-10-05 19:03:15 +0530

    add get image function

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -18,4 +20,17 @@ func Handler(event InputEvent) (string, error) {
 type InputEvent struct {
 	Link string `json: "link"`
 	Key  string `json: "key"`
+}
+
+func GetImage(url string) []byte {
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	defer res.Body.Close()
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	return bytes
 }
