c48c93d0581d35cfa09e0caa02421e6243fe038b
Spidey03 <sayhai2naveenkumar@gmail.com>
2021-10-05 19:04:12 +0530

    config s3 and region

--- main.go
+++ main.go
@@ -6,7 +6,25 @@ import (
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
+
+var (
+	s3session *s3.S3
+)
+
+const (
+	REGION      = "ap-south-1"
+	BUCKET_NAME = "golambdatest03"
+)
+
+func init() {
+	s3session = s3.New(session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(REGION),
+	})))
+}
 
 func main() {
 	lambda.Start(Handler)
